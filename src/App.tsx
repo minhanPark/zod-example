2975fd7f29788a7af884a2ebea763b55720debdf
Running water <alsgks1218@naver.com>
2024-09-11 11:09:36 +0900

    vite로 프로젝트 시작하고, tailwind, shadcn 추가

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+  const [step, setStep] = useState(1);
+  return (
+    <div className="">
+      <div className="">
+        <ul>
+          <li className="text-red-500">ssss</li>
+        </ul>
+      </div>
+      <div className="">폼 라인</div>
+    </div>
+  );
+}
+
+export default App;
