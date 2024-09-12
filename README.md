a04e82b006ce41b52e1f5ddc9f2f084089e86e61
minhanPark <alsgks1218@naver.com>
2024-09-12 10:47:20 +0900

    useFieldArray, coerce, refine 예시 추가

--- README.md
+++ README.md
@@ -79,3 +79,50 @@ export function Basic() {
   return <form onSubmit={formMethods.handleSubmit(onValid, onError)}></form>;
 }
 ```
+
+### 자주 쓰는 유형
+
+```ts
+// 문자열, 공백으로 넣어도 됨
+nickname: z.string()
+
+// 문자열중에 공백 허용 안되는 필수값
+name: z.string().min(1, { message: "name은 필수값입니다." })
+
+// 셀렉트 박스랑 같이 쓸 때 enum형태로 특정 value만 받을 수 있음
+bankName: z.enum(["", "신한", "국민", "우리"], {
+    message: "은행을 올바르게 선택해주세요.",
+  })
+
+// react-hook-form의 useFieldArray와 같이 쓸 때 타입 지정방법 및 refine을 통해서 커스텀 validation 추가 가능
+companyIndustry: z
+      .array(
+        z.object({
+          value: z.string().trim().min(1, {
+            message: "빈칸을 삭제해주시거나 대표자를 입력해주세요.",
+          }),
+        })
+      )
+      /** refine을 통해서 커스텀한 validation 추가 가능 */
+      .refine((value) => value.length < 3, {
+        message: "대표자는 2명까지만 입력가능합니다.",
+      }),
+
+// input값은 문자가 들어오지만 숫자를 받아야 할 때 coerce를 통해서 변환 가능
+employees: z.coerce.number({ message: "숫자만 입력해주세요." }),
+
+// 전체 객체에 refine을 붙여주면 data에 전체 값을 받아서 validation 가능
+const schema = z
+    password: z
+      .string()
+      /**trim으로 공백 삭제 */
+      .trim()
+      .min(6, { message: "비밀번호는 6자리 이상이어야 합니다." }),
+    passwordConfirm: z.string().trim(),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    path: ["passwordConfirm"],
+    message: "비밀번호가 일치하지 않습니다.",
+  });
+
+```
--- src/form/basic2.tsx
+++ src/form/basic2.tsx
@@ -7,7 +7,6 @@ import {
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-// coerce, refine(password), useFieldArray,
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -19,9 +18,35 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-const schema = z.object({
-  companyIndustry: z.array(z.object({ value: z.string().min(1) })),
-});
+const schema = z
+  .object({
+    /**[{ value: "" }, {value: ""}] 형태로 추가될 때 타입 지정방법 */
+    companyIndustry: z
+      .array(
+        z.object({
+          value: z.string().trim().min(1, {
+            message: "빈칸을 삭제해주시거나 대표자를 입력해주세요.",
+          }),
+        })
+      )
+      /** refine을 통해서 커스텀한 validation 추가 가능 */
+      .refine((value) => value.length < 3, {
+        message: "대표자는 2명까지만 입력가능합니다.",
+      }),
+    /**coerce.number로 들어오는 값에 Number(input) 형태처럼 처리가능하고 더 복잡한 형태는 transform을 사용하면 가능 */
+    employees: z.coerce.number({ message: "숫자만 입력해주세요." }),
+    /**trim으로 공백 삭제 */
+    password: z
+      .string()
+      .trim()
+      .min(6, { message: "비밀번호는 6자리 이상이어야 합니다." }),
+    passwordConfirm: z.string().trim(),
+  })
+  /** 전체 객체에 refine을 걸면 data 모든 곳에 접근 가능한 validation을 만들 수 있음 */
+  .refine((data) => data.password === data.passwordConfirm, {
+    path: ["passwordConfirm"],
+    message: "비밀번호가 일치하지 않습니다.",
+  });
 
 /**schema를 통해서 타입 만들기 */
 type Schema = z.infer<typeof schema>;
@@ -29,7 +54,10 @@ type Schema = z.infer<typeof schema>;
 export function Basic2() {
   const formMethods = useForm<Schema>({
     defaultValues: {
-      companyIndustry: [],
+      companyIndustry: [{ value: "" }],
+      employees: 0,
+      password: "",
+      passwordConfirm: "",
     },
     resolver: zodResolver(schema),
   });
@@ -55,10 +83,33 @@ export function Basic2() {
       </CardHeader>
       <CardContent className="space-y-2">
         <form onSubmit={formMethods.handleSubmit(onValid, onError)}>
-          <Label>회사 업종</Label>
+          <Label>대표자</Label>
           <div className="">
-            <Input />
+            {fields.map((item, index) => (
+              <div className="flex gap-2 mb-1.5" key={item.id}>
+                <Input
+                  className="flex-1"
+                  {...formMethods.register(`companyIndustry.${index}.value`)}
+                />
+                <Button disabled={index === 0} onClick={() => remove(index)}>
+                  삭제
+                </Button>
+              </div>
+            ))}
+            <Button
+              variant="link"
+              type="button"
+              onClick={() => append({ value: "" })}
+            >
+              + 대표자 추가
+            </Button>
           </div>
+          <Label>직원수</Label>
+          <Input {...formMethods.register("employees")} />
+          <Label>비밀번호</Label>
+          <Input type="password" {...formMethods.register("password")} />
+          <Label>비밀번호 확인</Label>
+          <Input type="password" {...formMethods.register("passwordConfirm")} />
           <Button className="mt-10 w-full">제출</Button>
         </form>
       </CardContent>
